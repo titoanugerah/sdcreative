ad705ff79093c69585c8ad60518c35cc09dc8478
titoanugerah <titoanugerah@student.undip.ac.id>
2019-07-14 22:10:24 +0700

    Added dashboard menu

---
+++ application/views/client/menu.php
@@ -0,0 +1,6 @@
+<li class="nav-item <?php if($content['view_name']=='dashboard'){echo 'active';} ?>">
+  <a  href="<?php echo base_url('dashboard'); ?>" >
+    <i class="fas fa-home"></i>
+    <p>Dashboard</p>
+  </a>
+</li>
