751d5a4016a5ddaebd54ad479be33d8182bb1463
titoanugerah <titoanugerah@student.undip.ac.id>
2019-07-25 13:45:05 +0700

    Fixed looping

--- application/views/staff/detailOrder.php
+++ application/views/staff/detailOrder.php
@@ -256,10 +256,7 @@
                             <div class="timeline-body">
                               <p><?php echo $content['order']->admin ?> selaku admin memilih petugas untuk project ini antara lain </p>
                               <div class="row">
-
-                                <?php for($i=1; $i<=$content['order']->pic_count; $i++){ ?>
-                                  <h5 class="card-title"><?php echo $content['order']->fullname_pic1.','.$content['order']->fullname_pic2; ?></h5>
-                                <?php } ?>
+                                <h5 class="card-title"><?php echo $content['order']->fullname_pic1.','.$content['order']->fullname_pic2; ?></h5>
                               </div>
                             </div>
                           </div>
